426ed796ed34fb8e97737dfbc073b2616272a2af
Andrew Gustafson <gus4000@gmail.com>
2020-06-18 16:58:34 +0100

    AutoAckingCallbackJob

--- core/src/main/scala/com/itv/scheduler/PublishCallbackJob.scala
+++ core/src/main/scala/com/itv/scheduler/PublishCallbackJob.scala
@@ -1,5 +1,7 @@
 package com.itv.scheduler
 
+import cats.data.Kleisli
+import cats.effect.ConcurrentEffect
 import cats.implicits._
 import org.quartz._
 
@@ -10,4 +12,12 @@ abstract class PublishCallbackJob extends Job {
 
   override def execute(context: JobExecutionContext): Unit =
     handleMessage(context).valueOr(throw _)
+}
+
+final class AutoAckingCallbackJob[F[_], A](emitMessage: Kleisli[F, A, Unit])(implicit
+    F: ConcurrentEffect[F],
+    jobDecoder: JobDecoder[A]
+) extends PublishCallbackJob {
+  override def handleMessage: JobExecutionContext => Either[Throwable, Unit] =
+    jobExecutionContext => F.toIO(jobDecoder(jobExecutionContext).liftTo[F] >>= emitMessage.run).attempt.unsafeRunSync()
 }
