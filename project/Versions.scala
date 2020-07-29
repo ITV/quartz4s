c1e1c0d272cb7035d87e8cbf1362bc58948c32f6
Scala Steward <itv-user-ci@itv.com>
2020-07-29 09:07:20 +0000

    Update scalamock to 5.0.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,6 +8,6 @@ object Versions {
   val quartz              = "2.3.2"
   val scalatest           = "3.1.2"
   val scalatestScalacheck = "3.1.0.+"
-  val scalamock           = "4.4.0"
+  val scalamock           = "5.0.0"
   val testContainers      = "0.36.1"
 }
