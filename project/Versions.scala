bb261dace8f207e2243694c3570f857c630743b1
itv-user-ci <52080055+itv-user-ci@users.noreply.github.com>
2020-09-03 13:51:28 +0100

    Update scalatest to 3.2.2 (#30)
    Co-authored-by: Scala Steward <itv-user-ci@itv.com>

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val logback             = "1.2.3"
   val postgresql          = "42.2.16"
   val quartz              = "2.3.2"
-  val scalatest           = "3.2.0"
+  val scalatest           = "3.2.2"
   val scalatestScalacheck = "3.2.1.0"
   val scalamock           = "5.0.0"
   val testContainers      = "0.38.1"
