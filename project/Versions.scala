8e8eca9eccc5adc12135969a8735d40e2815a60b
Scala Steward <itv-user-ci@itv.com>
2020-11-25 12:39:50 +0000

    Update flyway-core to 7.2.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val extruder            = "0.11.0"
-  val flyway              = "6.5.7"
+  val flyway              = "7.2.1"
   val fs2                 = "2.4.5"
   val hikari              = "3.4.5"
   val logback             = "1.2.3"
