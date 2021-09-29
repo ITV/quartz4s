671a94f13fc0509c6cf9aa591d8c9635ffcbb7d6
Andrew Gustafson <andrew.gustafson@itv.com>
2021-09-29 19:39:21 +1000

    Add JobCodec convenience methods (#186)
    - Add Functor[JobDecoder]; add JobCodec convenience methods
    - Add JobCodec.iemap method

--- core/src/main/scala/com/itv/scheduler/JobCodec.scala
+++ core/src/main/scala/com/itv/scheduler/JobCodec.scala
@@ -1,3 +1,26 @@
 package com.itv.scheduler
 
-trait JobCodec[A] extends JobDecoder[A] with JobDataEncoder[A]
+import cats.data.Chain
+import cats.{Contravariant, Functor, Invariant}
+
+trait JobCodec[A] extends JobDecoder[A] with JobDataEncoder[A] {
+  def iemap[B](f: A => Either[Throwable, B])(g: B => A): JobCodec[B] =
+    JobCodec.from(emap(f), Contravariant[JobDataEncoder].contramap(this)(g))
+}
+
+object JobCodec {
+  implicit val invariantInstance: Invariant[JobCodec] = new Invariant[JobCodec] {
+    override def imap[A, B](fa: JobCodec[A])(f: A => B)(g: B => A): JobCodec[B] =
+      JobCodec.from[B](Functor[JobDecoder].map(fa)(f), Contravariant[JobDataEncoder].contramap(fa)(g))
+  }
+
+  def from[A](implicit decodeA: JobDecoder[A], encodeA: JobDataEncoder[A]): JobCodec[A] = new JobCodec[A] {
+    override private[scheduler] def read(path: Chain[String], jobData: PartiallyDecodedJobData): Either[Throwable, A] =
+      decodeA.read(path, jobData)
+
+    override private[scheduler] def apply(a: A): Map[List[String], String] =
+      encodeA.apply(a)
+  }
+
+  def apply[A](implicit ev: JobCodec[A]): JobCodec[A] = ev
+}
