bccc6125f1cf77847756b486f723a006ce9c2f09
jbwheatley <jack.wheatley@itv.com>
2021-10-01 14:42:15 +0100

    uncomment tests

--- core/src/test/scala/com/itv/scheduler/Quartz4sConfigTest.scala
+++ core/src/test/scala/com/itv/scheduler/Quartz4sConfigTest.scala
@@ -1,52 +1,52 @@
-//package com.itv.scheduler
-//
-//import org.scalacheck._
-//import org.scalatest.flatspec.AnyFlatSpec
-//import org.scalatest.matchers.should.Matchers
-//import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
-//
-//class Quartz4sConfigTest extends AnyFlatSpec with Matchers with ScalaCheckDrivenPropertyChecks {
-//  behavior of "Quartz4sConfig"
-//
-//  val sizedStringGen =
-//    Gen.chooseNum[Int](8, 16).flatMap(num => Gen.buildableOfN[String, Char](num, Gen.asciiPrintableChar))
-//
-//  it should "load config correctly" in {
-//    forAll(
-//      Gen.posNum[Int],
-//      Gen.posNum[Int],
-//      sizedStringGen,
-//      sizedStringGen,
-//      sizedStringGen,
-//    ) { case (threadCount, maxConnections, jdbcUrl, username, password) =>
-//      val quartzConfig = Quartz4sConfig(
-//        JobStoreConfig(
-//          driverDelegateClass = classOf[org.quartz.impl.jdbcjobstore.PostgreSQLDelegate],
-//        ),
-//        ThreadPoolConfig(threadCount),
-//        DataSourceConfig(
-//          driverClass = classOf[org.postgresql.Driver],
-//          jdbcUrl = jdbcUrl,
-//          username = username,
-//          password = password,
-//          maxConnections = maxConnections,
-//        )
-//      )
-//
-//      val expectedProperties: Map[String, String] = Map(
-//        "org.quartz.jobStore.isClustered"         -> "true",
-//        "org.quartz.jobStore.driverDelegateClass" -> "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate",
-//        "org.quartz.threadPool.threadCount"       -> threadCount.toString,
-//        "org.quartz.jobStore.class"               -> "org.quartz.impl.jdbcjobstore.JobStoreTX",
-//        "org.quartz.jobStore.dataSource"          -> "ds",
-//        "org.quartz.dataSource.ds.provider"       -> "hikaricp",
-//        "org.quartz.dataSource.ds.driver"         -> "org.postgresql.Driver",
-//        "org.quartz.dataSource.ds.URL"            -> jdbcUrl,
-//        "org.quartz.dataSource.ds.user"           -> username,
-//        "org.quartz.dataSource.ds.password"       -> password,
-//        "org.quartz.dataSource.ds.maxConnections" -> maxConnections.toString,
-//      )
-//      quartzConfig.toPropertiesMap should contain theSameElementsAs expectedProperties
-//    }
-//  }
-//}
+package com.itv.scheduler
+
+import org.scalacheck._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class Quartz4sConfigTest extends AnyFlatSpec with Matchers with ScalaCheckDrivenPropertyChecks {
+  behavior of "Quartz4sConfig"
+
+  val sizedStringGen =
+    Gen.chooseNum[Int](8, 16).flatMap(num => Gen.buildableOfN[String, Char](num, Gen.asciiPrintableChar))
+
+  it should "load config correctly" in {
+    forAll(
+      Gen.posNum[Int],
+      Gen.posNum[Int],
+      sizedStringGen,
+      sizedStringGen,
+      sizedStringGen,
+    ) { case (threadCount, maxConnections, jdbcUrl, username, password) =>
+      val quartzConfig = Quartz4sConfig(
+        JobStoreConfig(
+          driverDelegateClass = classOf[org.quartz.impl.jdbcjobstore.PostgreSQLDelegate],
+        ),
+        ThreadPoolConfig(threadCount),
+        DataSourceConfig(
+          driverClass = classOf[org.postgresql.Driver],
+          jdbcUrl = jdbcUrl,
+          username = username,
+          password = password,
+          maxConnections = maxConnections,
+        )
+      )
+
+      val expectedProperties: Map[String, String] = Map(
+        "org.quartz.jobStore.isClustered"         -> "true",
+        "org.quartz.jobStore.driverDelegateClass" -> "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate",
+        "org.quartz.threadPool.threadCount"       -> threadCount.toString,
+        "org.quartz.jobStore.class"               -> "org.quartz.impl.jdbcjobstore.JobStoreTX",
+        "org.quartz.jobStore.dataSource"          -> "ds",
+        "org.quartz.dataSource.ds.provider"       -> "hikaricp",
+        "org.quartz.dataSource.ds.driver"         -> "org.postgresql.Driver",
+        "org.quartz.dataSource.ds.URL"            -> jdbcUrl,
+        "org.quartz.dataSource.ds.user"           -> username,
+        "org.quartz.dataSource.ds.password"       -> password,
+        "org.quartz.dataSource.ds.maxConnections" -> maxConnections.toString,
+      )
+      quartzConfig.toPropertiesMap should contain theSameElementsAs expectedProperties
+    }
+  }
+}
--- core/src/test/scala/com/itv/scheduler/QuartzTaskSchedulerTest.scala
+++ core/src/test/scala/com/itv/scheduler/QuartzTaskSchedulerTest.scala
@@ -1,87 +1,87 @@
-//package com.itv.scheduler
-//
-//import java.time.Instant
-//import java.util.Properties
-//import cats.effect._
-//import cats.effect.std.Queue
-//import cats.effect.unsafe.IORuntime
-//import cats.syntax.all._
-//import com.dimafeng.testcontainers._
-//import org.flywaydb.core.Flyway
-//import org.quartz.{CronExpression, JobKey, TriggerKey}
-//import org.scalatest.BeforeAndAfterEach
-//import org.scalatest.flatspec.AnyFlatSpec
-//import org.scalatest.matchers.should.Matchers
-//
-//import scala.concurrent.duration._
-//
-//class QuartzTaskSchedulerTest extends AnyFlatSpec with Matchers with ForAllTestContainer with BeforeAndAfterEach {
-//  override val container: PostgreSQLContainer = PostgreSQLContainer()
-//
-//  implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
-//
-//  lazy val quartzProperties: QuartzProperties = {
-//    val props = new Properties()
-//    props.setProperty("org.quartz.jobStore.isClustered", "true")
-//    props.setProperty("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate")
-//    props.setProperty("org.quartz.threadPool.threadCount", "4")
-//    props.setProperty("org.quartz.jobStore.class", "org.quartz.impl.jdbcjobstore.JobStoreTX")
-//    props.setProperty("org.quartz.jobStore.dataSource", "ds")
-//    props.setProperty("org.quartz.dataSource.ds.provider", "hikaricp")
-//    props.setProperty("org.quartz.dataSource.ds.driver", container.driverClassName)
-//    props.setProperty("org.quartz.dataSource.ds.URL", container.jdbcUrl)
-//    props.setProperty("org.quartz.dataSource.ds.user", container.username)
-//    props.setProperty("org.quartz.dataSource.ds.password", container.password)
-//    props.setProperty("org.quartz.dataSource.ds.maxConnections", "4")
-//    QuartzProperties(props)
-//  }
-//
-//  lazy val flyway: Flyway =
-//    Flyway.configure().dataSource(container.jdbcUrl, container.username, container.password).load()
-//
-//  override def beforeEach(): Unit = {
-//    println("Migrating db ...")
-//    flyway.migrate()
-//    println("Migrated db")
-//  }
-//
-//  behavior of "QuartzTaskScheduler"
-//
-//  def schedulerResource(
-//      messageQueue: Queue[IO, ParentTestJob]
-//  ): Resource[IO, QuartzTaskScheduler[IO, ParentTestJob]] =
-//    MessageQueueJobFactory.autoAcking[IO, ParentTestJob](messageQueue).flatMap { jobFactory =>
-//      QuartzTaskScheduler[IO, ParentTestJob](quartzProperties, jobFactory)
-//    }
-//
-//  it should "schedule jobs to run every second" in {
-//    val elementCount = 6
-//    val userJob      = UserJob(UserId("user-id-123"))
-//
-//    val result = Queue.unbounded[IO, ParentTestJob] >>= { messageQueue =>
-//      schedulerResource(messageQueue).use { scheduler =>
-//        scheduler
-//          .scheduleJob(
-//            JobKey.jobKey("child-object-job"),
-//            ChildObjectJob,
-//            TriggerKey.triggerKey("cron-test-trigger"),
-//            CronScheduledJob(new CronExpression("* * * ? * *"))
-//          )
-//          .flatTap(runTime => IO(println(s"Next cron job scheduled for $runTime"))) *>
-//          scheduler
-//            .scheduleJob(
-//              JobKey.jobKey("single-user-job"),
-//              userJob,
-//              TriggerKey.triggerKey("scheduled-single-test-trigger"),
-//              JobScheduledAt(Instant.now.plusSeconds(2))
-//            )
-//            .flatTap(runTime => IO(println(s"Next single job scheduled for $runTime"))) *>
-//          messageQueue.take.replicateA(elementCount)
-//      }
-//    }
-//    val messages = result.unsafeRunTimed(10.seconds).getOrElse(fail("Operation timed out completing"))
-//
-//    val expectedMessages: List[ParentTestJob] = List.fill[ParentTestJob](elementCount - 1)(ChildObjectJob) :+ userJob
-//    messages should contain theSameElementsAs expectedMessages
-//  }
-//}
+package com.itv.scheduler
+
+import java.time.Instant
+import java.util.Properties
+import cats.effect._
+import cats.effect.std.Queue
+import cats.effect.unsafe.IORuntime
+import cats.syntax.all._
+import com.dimafeng.testcontainers._
+import org.flywaydb.core.Flyway
+import org.quartz.{CronExpression, JobKey, TriggerKey}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+class QuartzTaskSchedulerTest extends AnyFlatSpec with Matchers with ForAllTestContainer with BeforeAndAfterEach {
+  override val container: PostgreSQLContainer = PostgreSQLContainer()
+
+  implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
+
+  lazy val quartzProperties: QuartzProperties = {
+    val props = new Properties()
+    props.setProperty("org.quartz.jobStore.isClustered", "true")
+    props.setProperty("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate")
+    props.setProperty("org.quartz.threadPool.threadCount", "4")
+    props.setProperty("org.quartz.jobStore.class", "org.quartz.impl.jdbcjobstore.JobStoreTX")
+    props.setProperty("org.quartz.jobStore.dataSource", "ds")
+    props.setProperty("org.quartz.dataSource.ds.provider", "hikaricp")
+    props.setProperty("org.quartz.dataSource.ds.driver", container.driverClassName)
+    props.setProperty("org.quartz.dataSource.ds.URL", container.jdbcUrl)
+    props.setProperty("org.quartz.dataSource.ds.user", container.username)
+    props.setProperty("org.quartz.dataSource.ds.password", container.password)
+    props.setProperty("org.quartz.dataSource.ds.maxConnections", "4")
+    QuartzProperties(props)
+  }
+
+  lazy val flyway: Flyway =
+    Flyway.configure().dataSource(container.jdbcUrl, container.username, container.password).load()
+
+  override def beforeEach(): Unit = {
+    println("Migrating db ...")
+    flyway.migrate()
+    println("Migrated db")
+  }
+
+  behavior of "QuartzTaskScheduler"
+
+  def schedulerResource(
+      messageQueue: Queue[IO, ParentTestJob]
+  ): Resource[IO, QuartzTaskScheduler[IO, ParentTestJob]] =
+    MessageQueueJobFactory.autoAcking[IO, ParentTestJob](messageQueue).flatMap { jobFactory =>
+      QuartzTaskScheduler[IO, ParentTestJob](quartzProperties, jobFactory)
+    }
+
+  it should "schedule jobs to run every second" in {
+    val elementCount = 6
+    val userJob      = UserJob(UserId("user-id-123"))
+
+    val result = Queue.unbounded[IO, ParentTestJob] >>= { messageQueue =>
+      schedulerResource(messageQueue).use { scheduler =>
+        scheduler
+          .scheduleJob(
+            JobKey.jobKey("child-object-job"),
+            ChildObjectJob,
+            TriggerKey.triggerKey("cron-test-trigger"),
+            CronScheduledJob(new CronExpression("* * * ? * *"))
+          )
+          .flatTap(runTime => IO(println(s"Next cron job scheduled for $runTime"))) *>
+          scheduler
+            .scheduleJob(
+              JobKey.jobKey("single-user-job"),
+              userJob,
+              TriggerKey.triggerKey("scheduled-single-test-trigger"),
+              JobScheduledAt(Instant.now.plusSeconds(2))
+            )
+            .flatTap(runTime => IO(println(s"Next single job scheduled for $runTime"))) *>
+          messageQueue.take.replicateA(elementCount)
+      }
+    }
+    val messages = result.unsafeRunTimed(10.seconds).getOrElse(fail("Operation timed out completing"))
+
+    val expectedMessages: List[ParentTestJob] = List.fill[ParentTestJob](elementCount - 1)(ChildObjectJob) :+ userJob
+    messages should contain theSameElementsAs expectedMessages
+  }
+}
