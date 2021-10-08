7b49212fb7c40183c85dfce2e47cf2838acea549
Scala Steward <me@scala-steward.org>
2021-10-08 10:33:03 +0200

    Update flyway-core to 8.0.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val catsEffect          = "3.2.9"
   val collectionCompat    = "2.5.0"
   val disciplineMunit     = "1.0.9"
-  val flyway              = "7.15.0"
+  val flyway              = "8.0.0"
   val hikari              = "5.0.0"
   val logback             = "1.2.6"
   val munit               = "0.7.29"
