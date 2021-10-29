e3350c7d096445fefb17791be196880f617df915
Scala Steward <me@scala-steward.org>
2021-10-30 01:57:35 +0200

    Update postgresql to 42.3.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val hikari              = "5.0.0"
   val logback             = "1.2.6"
   val munit               = "0.7.29"
-  val postgresql          = "42.2.24"
+  val postgresql          = "42.3.1"
   val quartz              = "2.3.2"
   val scalatest           = "3.2.10"
   val scalacheckShapeless = "1.3.0"
