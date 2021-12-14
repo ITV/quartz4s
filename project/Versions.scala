865a59c4dbfeaaf3bc3d517d48da3362d7edad3a
Scala Steward <me@scala-steward.org>
2021-12-14 17:04:32 +0100

    Update logback-classic to 1.2.8

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val disciplineMunit     = "1.0.9"
   val flyway              = "8.2.1"
   val hikari              = "5.0.0"
-  val logback             = "1.2.7"
+  val logback             = "1.2.8"
   val munit               = "0.7.29"
   val postgresql          = "42.3.1"
   val quartz              = "2.3.2"
