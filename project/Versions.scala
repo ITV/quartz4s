d00ecc9473debdf8500b5553428589f858a1f482
Scala Steward <scala_steward@virtuslab.com>
2022-08-26 15:02:11 +0000

    Update postgresql to 42.5.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val hikari              = "5.0.1"
   val logback             = "1.4.4"
   val munit               = "0.7.29"
-  val postgresql          = "42.3.7"
+  val postgresql          = "42.5.0"
   val quartz              = "2.3.2"
   val scalatest           = "3.2.14"
   val scalacheckShapeless = "1.3.0"
